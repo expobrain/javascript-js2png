d7eb0cf636a7cc3e5a98d231eeb0b10b035fa024
Frivolous-Sites <35827420+Frivolous-Sites@users.noreply.github.com>
2018-08-25 17:20:53 -0400

    Update loader.js
    · Convert to a function.

--- html/js/loader.js
+++ html/js/loader.js
@@ -1,48 +1,51 @@
 "use strict"
 
-var img = document.getElementById("payload");
+function js2png(imgID) {
+    var img = document.getElementById("payload");
 
-img.addEventListener("load", function () {
-    // Setup Canvas a draw image
-    var context, canvas;
+    img.addEventListener("load", function () {
+        // Setup Canvas a draw image.
+        var context, canvas;
 
-    canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+        canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-    context = canvas.getContext("2d");
-    context.drawImage(img, 0, 0);
+        context = canvas.getContext("2d");
+        context.drawImage(img, 0, 0);
 
-    // Read image's data
-    var data;
+        // Read image's data.
+        var data;
 
-    data = context.getImageData(0, 0, img.width, img.height).data;
+        data = context.getImageData(0, 0, img.width, img.height).data;
 
-    // Read size of the payload
-    var size = 0;
+        // Read size of the payload.
+        var size = 0;
 
-    size += data[1] << 56;
-    size += data[2] << 48;
-    size += data[4] << 40;
-    size += data[5] << 32;
-    size += data[6] << 24;
-    size += data[8] << 16;
-    size += data[9] << 8;
-    size += data[10];
+        size += data[1] << 56;
+        size += data[2] << 48;
+        size += data[4] << 40;
+        size += data[5] << 32;
+        size += data[6] << 24;
+        size += data[8] << 16;
+        size += data[9] << 8;
+        size += data[10];
 
-    // Read payload into string
-    var payload = '';
+        // Read payload into string.
+        var payload = '';
 
-    for (var i = 12; i < data.length; i++) {
-        if ((i + 1) % 4) {
-            var char = data[i];
+        for (var i = 12; i < data.length; i++) {
+            if ((i + 1) % 4) {
+                var char = data[i];
 
-            if (char >= 32) {  // Strip any non-ASCII to keep eval() happy
-                payload += String.fromCharCode(char);
+                if (char >= 32) {  
+                    // Strip any non-ASCII to keep eval() happy :D                 
+                    payload += String.fromCharCode(char);
+                }
             }
         }
-    }
 
-    // Execute code
-    eval(payload);
-}, false);
+        // Execute code.
+        eval(payload);
+    }, false);
+}
